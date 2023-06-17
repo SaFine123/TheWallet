3ee05e29f5b3113b6779e968948fa727fecd41eb
Douglas Queiroz <douglas.queiroz@retailinmotion.com>
2023-06-17 13:06:12 -0300

    Remove State compose

--- app/src/main/java/com/douglasqueiroz/thewallet/feature/currencylist/logic/CurrencyListViewModel.kt
+++ app/src/main/java/com/douglasqueiroz/thewallet/feature/currencylist/logic/CurrencyListViewModel.kt
@@ -1,8 +1,5 @@
 package com.douglasqueiroz.thewallet.feature.currencylist.logic
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.douglasqueiroz.thewallet.data.local.dao.CurrencyDao
@@ -15,7 +12,6 @@ import com.douglasqueiroz.thewallet.ui.components.OnTopBarClick
 import com.douglasqueiroz.thewallet.ui.components.OnTopBarClickImp
 import com.douglasqueiroz.thewallet.ui.navigation.NavRouter
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -28,10 +24,6 @@ class CurrencyListViewModel(
     OnBottomBarClick by OnBottomBarClickImpl(navRouter),
     OnTopBarClick by OnTopBarClickImp(navRouter)
 {
-
-    var state by mutableStateOf(CurrencyListViewState())
-    private set
-
     private val _stateFlow = MutableStateFlow(CurrencyListViewState())
     val stateFlow = _stateFlow.asStateFlow()
 
